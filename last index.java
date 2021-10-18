baea6be37a8957b0ce99135cff68451c999380cd
MaheshChand1528 <mannu44800@gmail.com>
2021-10-18 22:22:45 +0530

    pepcoding problems

--- last index.java
+++ last index.java
@@ -5,14 +5,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         // write your code here
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
         int[] a=new int[n];
         for(int i=0;i<n;i++)
         {
-            a[i]=s.nextInt();
+            a[i]=scn.nextInt();
         }
-        int x=s.nextInt();
+        int x=scn.nextInt();
         System.out.print(lastIndex(a,0,x));
     }
 
